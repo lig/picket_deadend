0d842e8da8bdfdf8558d0f9f6116a5a05b4b8af1
Serge Matveenko <s@matveenko.ru>
2009-05-04 22:35:38 +0400

    Filters functionality implemented

---
+++ filter/filters.py
@@ -0,0 +1,147 @@
+from datetime import datetime, timedelta
+
+from django import forms
+from django.db.models import Q
+from django.db.models.sql.constants import QUERY_TERMS
+from django.utils.translation import ugettext as _
+
+from filter.fields import RangeField, LookupTypeField
+
+__all__ = [
+    'Filter', 'CharFilter', 'BooleanFilter', 'ChoiceFilter',
+    'MultipleChoiceFilter', 'DateFilter', 'DateTimeFilter', 'TimeFilter',
+    'ModelChoiceFilter', 'ModelMultipleChoiceFilter', 'NumberFilter',
+    'RangeFilter', 'DateRangeFilter'
+]
+
+LOOKUP_TYPES = sorted(QUERY_TERMS.keys())
+
+class Filter(object):
+    creation_counter = 0
+    field_class = forms.Field
+
+    def __init__(self, name=None, label=None, widget=None, action=None,
+        lookup_type='exact', **kwargs):
+        self.name = name
+        self.label = label
+        if action:
+            self.filter = action
+        self.lookup_type = lookup_type
+        self.widget = widget
+        self.extra = kwargs
+
+        self.creation_counter = Filter.creation_counter
+        Filter.creation_counter += 1
+
+    @property
+    def field(self):
+        if not hasattr(self, '_field'):
+            if self.lookup_type is None or isinstance(self.lookup_type, (list, tuple)):
+                if self.lookup_type is None:
+                    lookup = [(x, x) for x in LOOKUP_TYPES]
+                else:
+                    lookup = [(x, x) for x in LOOKUP_TYPES if x in self.lookup_type]
+                self._field = LookupTypeField(self.field_class(required=False,
+                    widget=self.widget, **self.extra), lookup, required=False,
+                    label=self.label)
+            else:
+                self._field = self.field_class(required=False, label=self.label,
+                    widget=self.widget, **self.extra)
+        return self._field
+
+    def filter(self, qs, value):
+        if value:
+            if isinstance(value, (list, tuple)):
+                lookup = str(value[1])
+                value = value[0]
+            else:
+                lookup = self.lookup_type
+            return qs.filter(**{'%s__%s' % (self.name, lookup): value})
+        return qs
+
+class CharFilter(Filter):
+    field_class = forms.CharField
+
+class BooleanFilter(Filter):
+    field_class = forms.NullBooleanField
+
+    def filter(self, qs, value):
+        if value is not None:
+            return qs.filter(**{self.name: value})
+        return qs
+
+class ChoiceFilter(Filter):
+    field_class = forms.ChoiceField
+
+class MultipleChoiceFilter(Filter):
+    """
+    This filter preforms an OR query on the selected options.
+    """
+    field_class = forms.MultipleChoiceField
+    
+    """ @todo: contribute MultipleChoiceFilter fix to upstream """
+    def filter(self, qs, value):
+        if value:
+            q = Q()
+            for v in value:
+                q |= Q(**{self.name: v})
+            qs = qs.filter(q).distinct()
+        return qs
+
+class DateFilter(Filter):
+    field_class = forms.DateField
+
+class DateTimeFilter(Filter):
+    field_class = forms.DateTimeField
+
+class TimeFilter(Filter):
+    field_class = forms.TimeField
+
+class ModelChoiceFilter(Filter):
+    field_class = forms.ModelChoiceField
+
+class ModelMultipleChoiceFilter(MultipleChoiceFilter):
+    field_class = forms.ModelMultipleChoiceField
+
+class NumberFilter(Filter):
+    field_class = forms.DecimalField
+
+class RangeFilter(Filter):
+    field_class = RangeField
+
+    def filter(self, qs, value):
+        if value:
+            return qs.filter(**{'%s__range' % self.name: (value.start, value.stop)})
+        return qs
+
+class DateRangeFilter(ChoiceFilter):
+    options = {
+        '': (_('Any Date'), lambda qs, name: qs.all()),
+        1: (_('Today'), lambda qs, name: qs.filter(**{
+            '%s__year' % name: datetime.today().year,
+            '%s__month' % name: datetime.today().month,
+            '%s__day' % name: datetime.today().day
+        })),
+        2: (_('Past 7 days'), lambda qs, name: qs.filter(**{
+            '%s__gte' % name: (datetime.today() - timedelta(days=7)).strftime('%Y-%m-%d'),
+            '%s__lte' % name: datetime.today().strftime('%Y-%m-%d'),
+        })),
+        3: (_('This month'), lambda qs, name: qs.filter(**{
+            '%s__year' % name: datetime.today().year,
+            '%s__month' % name: datetime.today().month
+        })),
+        4: (_('This year'), lambda qs, name: qs.filter(**{
+            '%s__year' % name: datetime.today().year,
+        })),
+    }
+
+    def __init__(self, *args, **kwargs):
+        kwargs['choices'] = [(key, value[0]) for key, value in self.options.iteritems()]
+        super(DateRangeFilter, self).__init__(*args, **kwargs)
+
+    def filter(self, qs, value):
+        try:
+            value = int(value)
+        except (ValueError, TypeError):
+            value = ''
+        return self.options[value][1](qs, self.name)
