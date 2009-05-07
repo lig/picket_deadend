565d5829ea48a93fcd32e516f27240f0bf28cb14
Serge Matveenko <s@matveenko.ru>
2009-05-07 11:27:52 +0400

    Move filter lib into picket app

--- apps/picket/filter/fields.py
+++ apps/picket/filter/fields.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from filter.widgets import RangeWidget, LookupTypeWidget
+from widgets import RangeWidget, LookupTypeWidget
 
 class RangeField(forms.MultiValueField):
     widget = RangeWidget
