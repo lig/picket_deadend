e3d63ad41408962310ac4371dc99599d7c25e732
Serge Matveenko <s@matveenko.ru>
2010-12-27 18:58:41 +0300

    Add Issue Document.

--- apps/picket/documents.py
+++ apps/picket/documents.py
@@ -1,56 +1,77 @@
 from mongoengine import *
 
 
+class Employee(Document):
+    
+    user = ReferenceField('User')
+
+
 class Project(Document):
     
     name = StringField()
-    manager = ReferenceField('Employee')
-
-
-class Release(Document):
-    
-    name = StringField()
-    deadline = DateTimeField()
-    current_stage = ReferenceField('Stage')
-    milestones = SortedListField(EmbeddedDocumentField('Milestone'),
-        ordering='deadline')
-
-
-class Milestone(EmbeddedDocument):
-    
-    stage = ReferenceField('Stage')
-    deadline = DateTimeField()
+    manager = ReferenceField(Employee)
 
 
 class Department(Document):
     
     name = StringField()
-    head = ReferenceField('Employee')
+    head = ReferenceField(Employee)
+
+
+class Group(Document):
+    
+    department = ReferenceField(Department)
+    project = ReferenceField(Project)
+    employees = ListField(ReferenceField(Employee))    
 
 
 class Stage(Document):
     
     name = StringField()
-    department = ReferenceField('Department')
+    department = ReferenceField(Department)
     order = IntField()
     first = ListField(GenericReferenceField()) # Status, Stage
+
+
+class Milestone(EmbeddedDocument):
+    
+    stage = ReferenceField(Stage)
+    deadline = DateTimeField()
+
+
+class Release(Document):
+    
+    name = StringField()
+    project = ReferenceField(Project)
+    deadline = DateTimeField()
+    current_stage = ReferenceField(Stage)
+    milestones = SortedListField(EmbeddedDocumentField(Milestone),
+        ordering='deadline')
 
 
 class Status(Document):
     
     name = StringField()
-    department = ReferenceField('Department')
+    department = ReferenceField(Department)
     handler = GenericReferenceField() # Employee, Group, Department
     next = ListField(GenericReferenceField()) # Status, Stage
 
 
-class Group(Document):
+class Comment(EmbeddedDocument):
     
-    department = ReferenceField('Department')
-    project = ReferenceField('Project')
-    employees = ListField(ReferenceField('Employee'))    
+    submitted = DateTimeField()
+    author = ReferenceField(Employee)
+    text = StringField()
 
-
-class Employee(Document):
     
-    user = ReferenceField('User')
+class Issue(Document):
+    
+    subject = StringField()
+    submitted = DateTimeField()
+    author = ReferenceField(Employee)
+    text = StringField()
+    project = ReferenceField(Project)
+    status = ReferenceField(Status)
+    handler = GenericReferenceField() # Employee, Group, Department
+    comments = SortedListField(EmbeddedDocumentField(Comment),
+        ordering='submitted')
