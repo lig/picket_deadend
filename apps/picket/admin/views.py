a977c840b35a27a46f8c8fdd84d227e104a2243f
Serge Matveenko <s@matveenko.ru>
2011-05-04 23:29:38 +0400

    Drop unused copy import.

--- apps/picket/admin/views.py
+++ apps/picket/admin/views.py
@@ -17,8 +17,6 @@ You should have received a copy of the GNU General Public License
 along with Picket.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from copy import copy
-
 from django.conf import settings
 from django.contrib.messages import success, error
 from django.http import Http404, HttpResponseBadRequest
